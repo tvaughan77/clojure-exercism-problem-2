fbdbe2380591f69fbab4748c599c5b6548ab53be
tom.vaughan <tom@opower.com>
2014-10-16 19:50:33 -0400

    Replacing the split with a map of str

--- src/app/rna_transcription.clj
+++ src/app/rna_transcription.clj
@@ -1,6 +1,7 @@
 (ns app.rna_transcription
     (:gen-class))
 
+; Probably a better map-based way of doing this
 (defn base-pair [nucleotide]
     (cond
         (= "A" nucleotide) "U"
@@ -20,7 +21,7 @@
   (reduce str "" chars))
 
 (defn to-rna [sequence]
-    (make-str-from-chars (to-rna-internal (.split sequence "(?!^)"))))
+    (make-str-from-chars (to-rna-internal (map str sequence))))
 
 
 
